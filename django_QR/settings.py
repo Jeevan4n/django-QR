c8a460b6f15490a99ab6d8709776ea6ea180e1c3
Jeevan4n <jeevanskumar444@gmail.com>
2025-03-11 20:09:50 +0530

    qr-added

--- django_QR/settings.py
+++ django_QR/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,9 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+# If your static files are inside the main project directory
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# If you need to serve user-uploaded files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- django_QR/views.py
+++ django_QR/views.py
@@ -1,4 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .forms import QRCodeForm
+import qrcode
+import os
+from django.conf import settings
 
 def generate_qr_code(request):
-    return render(request,'generate_qr_code.html')
+    if request.method == 'POST':
+        form = QRCodeForm(request.POST)
+        if form.is_valid():
+            res_name = form.cleaned_data['restaurant_name']
+            url = form.cleaned_data['url']
+
+            # Generate QR code image using qrcode module
+            qr = qrcode.make(url)
+
+            # Create a filename based on the restaurant name
+            file_name = res_name.replace(" ", "_").lower() + '_menu.png'
+
+            # Define the folder inside static where the QR code will be saved
+            qr_folder = os.path.join(settings.BASE_DIR, 'static', 'qr_codes')
+            if not os.path.exists(qr_folder):
+                os.makedirs(qr_folder)
+            
+            # Complete file path
+            file_path = os.path.join(qr_folder, file_name)
+
+            # Save the QR code image to the file path
+            qr.save(file_path)
+
+            # Build the URL to access the QR code image
+            qr_code_url = '/static/qr_codes/' + file_name
+
+            # Pass the restaurant name and QR code URL to the template
+            context = {
+                'res_name': res_name,
+                'qr_code_url': qr_code_url,
+            }
+            return render(request, 'qr_result.html', context)
+    else:
+        form = QRCodeForm()
+
+    context = {'form': form}
+    return render(request, 'generate_qr_code.html', context)
